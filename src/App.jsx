a751f9ce481b01a8c3e1505899a3db43cd23c52f
weyinmiiiiii-pixel <weyinmiiiiii@gmail.com>
2026-06-24 15:10:23 +0100

    weyinmi

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import {BrowserRouter, Router} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 function App(){
   return(
     <Router>
       <div className="App">
         <Routes>
-    <h2>Welcome to LoanAptech</h2>
-  });
-  </Routes>
-  </div>
-  </Router>
+        <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./About.css";
+function About()  {
+    return (
+    <div className="about-page">
+    <div className="about-container">
+    <h1>About Us</h1>
+    <p>We are a trusted platform dedicated to making loan access simple,fast and transparent.Our mission is to
+     empower individuals ans small business with flexible financing options without the hassle of traditional
+     banking bureucracy</p>
+    <div className="features">
+    <div className="feature">
+    <h2>Fast Approval</h2>
+    <p>Get decisions in minutes, not weeks</p>
+         </div>
+    <div className="feature">
+    <h2>No hidden Fees</h2>
+    <p>100% free to aply - no hidden charges</p>
+         </div>
+    <div className="feature">
+    <h2>Secure & Private</h2>
+    <p>Your data is encrypted and never shared </p>
+         </div>
+  </div>
+        </div>
+        </div>
+    );
+} 
+export default About;
