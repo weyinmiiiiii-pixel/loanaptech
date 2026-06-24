916b36e3b0638304ad8f41f439388e62c4ee934f
weyinmiiiiii-pixel <weyinmiiiiii@gmail.com>
2026-06-24 12:56:16 +0100

    others

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {BrowserRouter, Router} from "react-router-dom";
+function App(){
+  return(
+    <Router>
+      <div className="App">
+        <Routes>
+    <h2>Welcome to LoanAptech</h2>
+  });
+  </Routes>
+  </div>
+  </Router>
