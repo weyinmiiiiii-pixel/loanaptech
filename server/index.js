38bef102b9c43a1ed4e8a947a1222cd21a9dcd89
weyinmiiiiii-pixel <weyinmiiiiii@gmail.com>
2026-07-08 13:48:06 +0100

    index

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser'); 
+require('dotenv').config();
+const connectDB = require('./config/db');
+
+const app = express();
+
+
+connectDB();
+
+// Middleware
+app.use(cors({
+   origin: [
+    "http://localhost:5173",
+    "https://loanaptech-orcin.vercel.app"
+  ],
+  credentials: true 
+}));
+app.use(express.json());
+app.use(cookieParser()); 
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/loans', require('./routes/loans'));
+
+// Test route
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'API is working!' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(` Server running on port ${PORT}`);
+});
