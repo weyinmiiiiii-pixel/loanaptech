7a9b3dd96a0635bce108c43edc8584ac8c27aa67
weyinmiiiiii-pixel <weyinmiiiiii@gmail.com>
2026-07-13 12:47:52 +0100

    change

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -43,7 +43,7 @@ const Signup = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("https://loanaptech-ax75.onrender.com/api/auth/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
